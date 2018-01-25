dd29af7cd8c4a13f9b80061b273a3df26952c5b8
Daniel Jette <daniel.jette@shopify.com>
2018-01-25 00:06:42 -0500

    CORS

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,8 +1,17 @@
 class StatusController < ApplicationController
 
+	after_action :set_headers
+
 	def show
 		respond_to do |format|
-			format.json { render :json => "{ Success }" }
+			format.json {
+				render :json => "{ Success }" 
+			}
 		end
 	end
+
+	protected
+		def set_headers
+			response.headers['Access-Control-Allow-Origin:'] = '*'
+		end
 end
