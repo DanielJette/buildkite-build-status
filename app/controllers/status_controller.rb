a660f6601de614f454a694d11408d69e3a39becf
Daniel Jette <daniel.jette@shopify.com>
2018-01-25 00:42:57 -0500

    Basically working

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,11 +1,26 @@
+require 'net/http'
+require 'json'
+
 class StatusController < ApplicationController
 
 	after_action :set_headers
 
 	def show
+
+		id = request.query_parameters['id']
+		url = 'https://badge.buildkite.com/' + id + '.json'
+		uri = URI(url)
+
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		response = http.get(uri)
+
+		json = JSON.parse(response.body)
+
 		respond_to do |format|
 			format.json {
-				render :json => "{ Success }" 
+				render :json => json 
 			}
 		end
 	end
