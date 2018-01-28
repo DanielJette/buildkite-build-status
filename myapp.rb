f8ec036a533274d06c6291e8536122308e2ab742
Daniel Jette <daniel.jette@shopify.com>
2018-01-28 13:11:30 -0500

    Convert to Sinatra app

---
+++ myapp.rb
@@ -0,0 +1,54 @@
+require 'sinatra'
+require 'net/http'
+require 'json'
+
+BUILDKITE_URL = "https://badge.buildkite.com/"
+
+private def fetch_status(id, branch)
+	uri = URI("#{BUILDKITE_URL}#{id}.json?branch=#{branch}")
+
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	response = http.get(uri)
+	json = JSON.parse(response.body)
+
+	if json['status'] == 'passing'
+		'green'
+	else
+		'red'
+	end
+end
+
+get '/status' do
+	if(!params.has_key?(:id))
+		halt 500, "500: BuildKite ID required"
+	end
+
+	branch = params[:branch] || 'master'
+	fetch_status(params[:id], branch)
+end
+
+get '/image' do
+	if(!params.has_key?(:id))
+		halt 500, "500: BuildKite ID required"
+	end
+
+	id = params[:id]
+	platform = params[:platform]  || 'android'
+	branch = params[:branch] || 'master'
+
+	"#{platform}-#{fetch_status(id, branch)}.png"
+end
+
+
+get '/badge' do
+	if(!params.has_key?(:id))
+		halt 500, "500: BuildKite ID required"
+	end
+
+	id = params[:id]
+	branch = params[:branch] || 'master'
+
+	"#{BUILDKITE_URL}#{id}.json?branch=#{branch}"
+end
